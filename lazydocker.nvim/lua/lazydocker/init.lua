cb28f1ee195dc663cbb50febdb605939d3bd2e89
honey <44633479+HoneyChasey@users.noreply.github.com>
2026-05-28 10:43:07 +0200

    Fix: windows kill working now

--- lazydocker.nvim/lua/lazydocker/init.lua
+++ lazydocker.nvim/lua/lazydocker/init.lua
@@ -1,12 +1,9 @@
--- lazydocker.nvim/lua/lazydocker/init.lua --
 local M = {}
 
 function M.open()
-  -- Create an empty buffer
   local buf = vim.api.nvim_create_buf(false, true)
 
-  -- Open a floating window in the center
-  vim.api.nvim_open_win(buf, true, {
+  local win = vim.api.nvim_open_win(buf, true, {
     relative = "editor",
     width  = math.floor(vim.o.columns * 0.9),
     height = math.floor(vim.o.lines   * 0.9),
@@ -16,8 +13,13 @@ function M.open()
     border = "rounded",
   })
 
-  -- Run lazydocker inside it
-  vim.fn.jobstart("lazydocker", { term = true })
+  vim.fn.jobstart("lazydocker", {
+    term = true,
+    on_exit = function()
+      vim.api.nvim_win_close(win, true)
+    end
+  })
+
   vim.cmd("startinsert")
 end
 
